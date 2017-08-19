73d712cc797c32b364d935a3b5fdeed591b4c683
Rose Wan <rose.wan@me.com>
2017-08-19 13:57:38 -0400

    dotenv and process.env for DEV ID and CLIENT ID not working but added.

--- README.md
+++ README.md
@@ -7,5 +7,11 @@ Just clone it, cd into it, npm install it, and npm start it.
 git clone git https://github.com/leebrandt/simple-react-seed.git [some new project name]
 cd [some new project name]
 npm i
-npm start
+~~npm start~~
+Run this in command line instead:
+DEV_ID=dev_id CLIENT_ID=client_id npm start
+
+(fill with your own Okta ID's)
 ```
+
+React app created from this activity: https://developer.okta.com/blog/2017/03/30/react-okta-sign-in-widget#the-loginpage-component
--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
+import '@okta/okta-signin-widget/dist/css/okta-theme.css';
+import OktaSignIn from '@okta/okta-signin-widget';
 
 export default class LoginPage extends React.Component{
+  constructor(){
+    super();
+    this.state = { user: null };
+    this.widget = new OktaSignIn({
+      baseUrl: `https://dev-${process.env.DEV_ID}.oktapreview.com`,
+      clientId: process.env.CLIENT_ID,
+      redirectUri: 'http://localhost:3000',
+      authParams: {
+        responseType: 'id_token'
+      }
+    });
+    console.log('DEV_ID ', process.env.DEV_ID);
+    console.log('CLIENT_ID ', process.env.CLIENT_ID);
+
+    this.showLogin = this.showLogin.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount(){
+    this.widget.session.get((response) => {
+      if(response.status !== 'INACTIVE'){
+        this.setState({user:response.login});
+      }else{
+        this.showLogin();
+      }
+    });
+  }
+
+  showLogin(){
+    Backbone.history.stop();
+    this.widget.renderEl({el:'#okta-login-container'},
+      (response) => {
+        this.setState({user: response.claims.email});
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  logout(){
+    this.widget.signOut(() => {
+      this.setState({user: null});
+      this.showLogin();
+    });
+  }
+
   render(){
     return(
-      <div>Login Page</div>
+      <div>
+        {this.state.user ? (
+          <div className="container">
+            <div>Welcome, {this.state.user}!</div>
+            <button onClick={this.logout}>Logout</button>
+          </div>
+        ) : null}
+        {this.state.user ? null : (
+          <div id="okta-login-container" className="login" />
+        )}
+      </div>
     );
   }
 }
